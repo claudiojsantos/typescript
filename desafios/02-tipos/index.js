78b1119a0e4acef2c3b935e0ddb3680941ceeef5
Cláudio Santos <cljos@hotmail.com>
2022-01-13 23:17:42 -0300

    chore: challenge 02 - data type conversion

---
+++ desafios/02-tipos/index.js
@@ -0,0 +1,3 @@
+var num1 = 20;
+var num1Str = num1.toString();
+console.log("O numero que foi convertido de number para string foi ".concat(num1Str));
---
+++ desafios/02-tipos/index.ts
@@ -0,0 +1,4 @@
+const num1: number = 20;
+const num1Str = num1.toString();
+
+console.log(`O numero que foi convertido de number para string foi ${num1Str}`);
