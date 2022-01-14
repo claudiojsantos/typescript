e932a265d78539cb4c99925d56d0034e2a95b039
Cláudio Santos <cljos@hotmail.com>
2022-01-13 22:25:00 -0300

    chore: type number

---
+++ 02-fundamentos/index.js
@@ -0,0 +1,7 @@
+var num1 = 10;
+console.log(num1);
+console.log(typeof num1);
+var num2 = 13.435544;
+console.log(num2);
+console.log(typeof num2);
+console.log(num2.toPrecision(4));
---
+++ 02-fundamentos/index.ts
@@ -0,0 +1,8 @@
+let num1: number = 10;
+console.log(num1);
+console.log(typeof num1);
+
+let num2: number = 13.435544;
+console.log(num2);
+console.log(typeof num2);
+console.log(num2.toPrecision(4));
