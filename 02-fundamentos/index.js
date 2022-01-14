7abaf08e33c7622dab309f4a7310e2ff5c3ea957
Cláudio Santos <cljos@hotmail.com>
2022-01-13 22:35:54 -0300

    chore: type string

--- 02-fundamentos/index.js
+++ 02-fundamentos/index.js
@@ -5,3 +5,10 @@ var num2 = 13.435544;
 console.log(num2);
 console.log(typeof num2);
 console.log(num2.toPrecision(4));
+// Strings
+var firstName = "Cláudio";
+var lastName = "Santos";
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName + " " + lastName);
+console.log(typeof firstName);
--- 02-fundamentos/index.ts
+++ 02-fundamentos/index.ts
@@ -6,3 +6,15 @@ let num2: number = 13.435544;
 console.log(num2);
 console.log(typeof num2);
 console.log(num2.toPrecision(4));
+
+// Strings
+
+let firstName: string = "Cláudio";
+let lastName: string = "Santos";
+
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+
+console.log(firstName + " " + lastName);
+
+console.log(typeof firstName);
