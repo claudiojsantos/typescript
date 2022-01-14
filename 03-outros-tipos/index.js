5b1e53e827c8298246b09187714ab922d56447ed
Cláudio Santos <cljos@hotmail.com>
2022-01-13 23:27:24 -0300

    chore: type array

---
+++ 03-outros-tipos/index.js
@@ -0,0 +1,8 @@
+var numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+console.log(numeros[2]);
+numeros.push(6);
+console.log(numeros);
+numeros.reverse();
+console.log(numeros);
+console.log(typeof numeros);
---
+++ 03-outros-tipos/index.ts
@@ -0,0 +1,12 @@
+let numeros: number[] = [1, 2, 3, 4, 5];
+
+console.log(numeros);
+console.log(numeros[2]);
+
+numeros.push(6);
+console.log(numeros);
+
+numeros.reverse();
+console.log(numeros);
+
+console.log(typeof numeros);
