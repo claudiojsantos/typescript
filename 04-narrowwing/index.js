d9a571edde3646aac911d9dc92490db3c2eb5908
Cláudio Santos <cljos@hotmail.com>
2022-01-15 20:26:12 -0300

    chore: narrowing - verify if value exists

--- 04-narrowwing/index.js
+++ 04-narrowwing/index.js
@@ -15,3 +15,22 @@ function sum(a, b) {
 sum("12", "21");
 sum(14, 19.32);
 sum("10", 20);
+// verify if value exists
+function operations(values, operation) {
+    if (operation) {
+        if (operation === "sum") {
+            var sum_1 = values.reduce(function (ant, act) { return ant + act; }, 0);
+            console.log(sum_1);
+        }
+        else if (operation === "multiply") {
+            var multiply = values.reduce(function (ant, act) { return ant * act; });
+            console.log(multiply);
+        }
+    }
+    else {
+        console.log("Defined a type operation");
+    }
+}
+operations([1, 2, 3]);
+operations([1, 2, 3], "sum");
+operations([2, 4, 6], "multiply");
--- 04-narrowwing/index.ts
+++ 04-narrowwing/index.ts
@@ -17,3 +17,23 @@ function sum(a: number | string, b: number | string) {
 sum("12", "21");
 sum(14, 19.32);
 sum("10", 20);
+
+// verify if value exists
+
+function operations(values: number[], operation?: string | undefined) {
+  if (operation) {
+    if (operation === "sum") {
+      const sum = values.reduce((ant, act) => ant + act, 0);
+      console.log(sum);
+    } else if (operation === "multiply") {
+      const multiply = values.reduce((ant, act) => ant * act);
+      console.log(multiply);
+    }
+  } else {
+    console.log("Defined a type operation");
+  }
+}
+
+operations([1, 2, 3]);
+operations([1, 2, 3], "sum");
+operations([2, 4, 6], "multiply");
