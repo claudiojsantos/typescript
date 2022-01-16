4b9b9c9061ee3228bc50519b957b99551bca53e7
Cláudio Santos <cljos@hotmail.com>
2022-01-15 22:53:29 -0300

    chore: functions - callback as argument

--- 05-functions/index.js
+++ 05-functions/index.js
@@ -3,3 +3,14 @@ function withoutReturn() {
     console.log("Function without return");
 }
 withoutReturn();
+// callback as argument
+function greetings(name) {
+    return "Welcome ".concat(name);
+}
+function preGreetings(f, userName) {
+    var greet = f(userName);
+    console.log(greet);
+}
+preGreetings(greetings, "Cláudio");
+preGreetings(greetings, "Antonio");
+preGreetings(greetings, "Aldo");
--- 05-functions/index.ts
+++ 05-functions/index.ts
@@ -5,3 +5,19 @@ function withoutReturn(): void {
 }
 
 withoutReturn();
+
+// callback as argument
+
+function greetings(name: string): string {
+  return `Welcome ${name}`;
+}
+
+function preGreetings(f: (name: string) => string, userName: string): void {
+  const greet = f(userName);
+
+  console.log(greet);
+}
+
+preGreetings(greetings, "Cláudio");
+preGreetings(greetings, "Antonio");
+preGreetings(greetings, "Aldo");
