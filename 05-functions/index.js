45205e71ac82c4f595d275917c6220a91e5963ad
Cláudio Santos <cljos@hotmail.com>
2022-01-16 01:49:26 -0300

    chore: functions - generics

--- 05-functions/index.js
+++ 05-functions/index.js
@@ -1,4 +1,15 @@
 // void
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 function withoutReturn() {
     console.log("Function without return");
 }
@@ -14,3 +25,13 @@ function preGreetings(f, userName) {
 preGreetings(greetings, "Cláudio");
 preGreetings(greetings, "Antonio");
 preGreetings(greetings, "Aldo");
+// Generics
+function viewFirstElement(array) {
+    return array[0];
+}
+console.log(viewFirstElement([1, 2, 3, 4]));
+console.log(viewFirstElement(["a", "b", "c", "d"]));
+function mergeObjects(obj1, obj2) {
+    return __assign(__assign({}, obj1), obj2);
+}
+console.log(mergeObjects({ nome: "claudio" }, { idade: 47 }));
--- 05-functions/index.ts
+++ 05-functions/index.ts
@@ -21,3 +21,21 @@ function preGreetings(f: (name: string) => string, userName: string): void {
 preGreetings(greetings, "Cláudio");
 preGreetings(greetings, "Antonio");
 preGreetings(greetings, "Aldo");
+
+// Generics
+
+function viewFirstElement<T>(array: T[]) {
+  return array[0];
+}
+
+console.log(viewFirstElement([1, 2, 3, 4]));
+console.log(viewFirstElement(["a", "b", "c", "d"]));
+
+function mergeObjects<U, T>(obj1: U, obj2: T) {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
+
+console.log(mergeObjects({ nome: "claudio" }, { idade: 47 }));
